96bc3611ca7b809392dc403148bfb54c342cdeb2
abubakarsial1234 <abubakarsial@Sial-pc.localdomain>
2025-09-05 16:22:16 +0500

    workflow

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 @app.route('/api/weather')
 def get_weather():
     weather_data = {"city": "Lahore", "temperature": 34, "condition": "Sunny"}
-    return jsonify(weather_data)
+    return jsonify(weather_data)   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
