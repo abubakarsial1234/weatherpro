12acf2edeb03e07018c2f1ba1fe183ca0ca12270
abubakarsial1234 <abubakarsial@Sial-pc.localdomain>
2025-09-05 17:43:53 +0500

    instance type changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -201,7 +201,7 @@ variable "key_name" {
 
 resource "aws_instance" "frontend" {
   ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
+  instance_type = "t3.small"
   subnet_id     = aws_subnet.public_1.id
   key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
@@ -211,7 +211,7 @@ resource "aws_instance" "frontend" {
 
 resource "aws_instance" "backend" {
   ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
+  instance_type = "t3.small"
   subnet_id     = aws_subnet.public_2.id
   key_name      = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
